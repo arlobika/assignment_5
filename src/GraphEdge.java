6c6474905262e289d076125c6fa844958d5081af
Selo <seloeldekci@gmail.com>
2024-11-27 20:21:04 -0500

    Update GraphEdge.java

--- src/GraphEdge.java
+++ src/GraphEdge.java
@@ -1,37 +1,41 @@
 
 public class GraphEdge {
 
-//	I need an origin and a destination
-//	I need an int type variable
-//	and I need a string label
-	
+	private GraphNode origin;
+	private GraphNode destination;
+	private int type;
+	private String label;
+	//constructor
 	public GraphEdge(GraphNode u, GraphNode v, int type, String label) {
-//		I should probably initialize everything
+		origin = u;
+		destination = v;
+		this.type = type;
+		this.label = label;
 	}
 	
-//	I should probably fill in the bodies of those setters and getters
+//getters and setters
 	public GraphNode firstEndpoint() {
-		
+		return origin;
 	}
 	
 	public GraphNode secondEndpoint() {
-		
+		return destination;
 	}
 	
 	public int getType() {
-		
+		return this.type;
 	}
 	
-	public void setype(int type) {
-		
+	public void setType(int newType) {
+		this.type = newType;
 	}
 	
 	public String getLabel() {
-		
+		return this.label;
 	}
 	
-	public void setLabel(String label) {
-		
+	public void setLabel(String newLabel) {
+		this.label = newLabel;
 	}
 	
 }
