267960c2e364ab2f3fe68cf436d46f1bda1cc4b0
Selo <seloeldekci@gmail.com>
2024-11-29 21:15:30 -0500

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -7,17 +7,6 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Maze {
-
-	
-//	instance variables you may need
-//	a variable storing the graph, a variable storing the id of the starting node, a variable storing the id of the end node
-//	a variable storing the read number of coins, maybe even a variable storing the path so far so that you don't perform accidental
-//	(and unnecessary cycles).
-//	if you maintain nodes on a path in a list, be careful to make a list of GraphNodes,
-//	otherwise removal from the list is going to behave in a weird way. 
-//	REMEMBER your nodes have a field mark.. maybe that field could be useful to avoid cycles...
-//	you may also want to have a variable storing the number of coins you have collected so far
-
 	// the graph of the maze
 	private Graph graph;
 	 // The starting node
@@ -38,22 +27,10 @@ public class Maze {
 	 */
 
 	public Maze(String inputFile) throws MazeException {
-//		initialize your graph variable by reading the input file!
-//		to maintain your code as clean and easy to debug as possible use the provided private helper method
-//		readInput to read the input file and initialize your graph variable
 		//initialize the path
 		path = new ArrayList<>();
 		//initialize the coins collected
 		coinsCollected = 0;
-		//initialize the graph
-		graph = new Graph(1);
-		//initialize the start
-		start = 0;
-		//initialize the end
-		end = 0;
-		//initialize the coins
-		coins = 0;
-		//read the input file
 		try {
 			BufferedReader inputReader = new BufferedReader(new FileReader(new File(inputFile)));
 			readInput(inputReader);
@@ -64,7 +41,7 @@ public class Maze {
 	}
 
 	/**
-	 * Get the graph
+	 * returns the graph
 	 * @return the graph
 	 */
 	public Graph getGraph() {
@@ -73,8 +50,8 @@ public class Maze {
 	}
 
 	/**
-	 * Solve the maze
-	 * @return the path
+	 * Solve the maze using DFS
+	 * @return the path iterator or null if no path was found
 	 */
 	public Iterator<GraphNode> solve() {
 //		simply call your private DFS. If you come up with a different approach that's ok too.
@@ -87,8 +64,7 @@ public class Maze {
 	}
 
 	/**
-	 * Get the number of coins
-	 * @return the number of coins
+	 * Depth first search to solve the maze
 	 */
 
 	private Iterator<GraphNode> DFS(int k, GraphNode go) throws GraphException {
@@ -104,34 +80,24 @@ public class Maze {
 		//get the edges
 		Iterator<GraphEdge> edges = graph.incidentEdges(go);
 		//iterate over the edges
-		while (edges.hasNext()) {
+		while (edges != null && edges.hasNext()) {
 			GraphEdge edge = edges.next();
 			//get the node
 			GraphNode node = edge.secondEndpoint();
 			//check if the node is marked
 			if (!node.isMarked()) {
-				//check if the node has coins
-				if (node.getName() == coins) {
-					//collect the coins
-					coinsCollected++;
-				}
-				//check if the node has been visited
-				if (!path.contains(node)) {
-					//check if the node has been visited
-					if (coinsCollected < k) {
-						//recursive call
-						Iterator<GraphNode> path = DFS(k, node);
-						//check if the path is not null
-						if (path != null) {
-							//return the path
-							return path;
-						}
+				int coinsNeeded = edge.getType();
+				if (coinsNeeded <= k) {
+					//recursive call
+					Iterator<GraphNode> result = DFS(k - coinsNeeded, node);
+					if (result != null) {
+						return result;
 					}
 				}
 			}
 		}
 		//remove the node from the path
-		path.remove(go);
+		path.remove(path.size() - 1);
 		//unmark the node
 		go.mark(false);
 		//return null
@@ -139,8 +105,8 @@ public class Maze {
 	}
 
 	/**
-	 * Get the number of coins
-	 * @return the number of coins
+	 * Read the input
+	 * and initialize the graph
 	 */
 
 	private void readInput(BufferedReader inputReader) throws IOException, GraphException {
@@ -149,70 +115,86 @@ public class Maze {
 //		remember to identify the starting and ending rooms
 //		The input will have size A + A-1 and L + L-1 because every pair of nodes has its relationship inbetween them in the textual representation!
 //		To maintain this method cleaner, you may use the private helper method insertEdge
-
 		//read the number of nodes
-		int numNodes = Integer.parseInt(inputReader.readLine());
-		//initialize the graph
-		graph = new Graph(numNodes);
-		//read the starting node
-		start = Integer.parseInt(inputReader.readLine());
-		//read the ending node
-		end = Integer.parseInt(inputReader.readLine());
-		//read the number of coins
-		coins = Integer.parseInt(inputReader.readLine());
-		//read the edges
-		for (int i = 0; i < numNodes; i++) {
-			//read the line
-			String line = inputReader.readLine();
-			//split the line
-			String[] values = line.split(" ");
-			//iterate over the values
-			for (int j = 0; j < values.length; j++) {
-				//check if the value is not 0
-				if (!values[j].equals("0")) {
-					//insert the edge
-					insertEdge(i, j, Integer.parseInt(values[j]), "");
+		int scaleFactor = Integer.parseInt(inputReader.readLine().trim());
+		int width = Integer.parseInt(inputReader.readLine().trim());
+		int length = Integer.parseInt(inputReader.readLine().trim());
+		coins = Integer.parseInt(inputReader.readLine().trim());
+
+		graph = new Graph(width * length);
+		String[] mazeLines = inputReader.lines().toArray(String[]::new);
+
+		for (int row = 0; row < mazeLines.length; row += 2) {
+			String rooms = mazeLines[row];
+			if (row + 1 < mazeLines.length) {
+				String walls = mazeLines[row + 1];
+
+				for (int col = 0; col < rooms.length(); col++) {
+					char roomChar = rooms.charAt(col);
+					handleRoom(roomChar, row / 2, col, width);
+
+					if (col < walls.length()) {
+						char wallChar = walls.charAt(col);
+						handleEdge(roomChar, wallChar, row / 2, col, width);
+					}
 				}
 			}
 		}
-
-		//close the reader
-		inputReader.close();
 	}
 
+	/*
+	 * Handle the room
+	 * @param roomChar the room character
+	 * @param row the row
+	 * @param col the column
+	 * @param width the width
+	 * @throws GraphException if the room is invalid
+	 */
+	private void handleRoom(char roomChar, int row, int col, int width) throws GraphException {
+		int nodeIndex = row * width + col;
+		switch (roomChar) {
+			case 's':
+				start = nodeIndex;
+				break;
+			case 'x':
+				end = nodeIndex;
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Handle the edge
+	 * @param roomChar the room character
+	 * @param wallChar the wall character
+	 * @param row the row
+	 * @param col the column
+	 * @param width the width
+	 * @throws GraphException if the edge is invalid
+	 */
+	private void handleEdge(char roomChar, char wallChar, int row, int col, int width) throws GraphException {
+		int nodeIndex = row * width + col;
+		int neighbourIndex = nodeIndex + 1;
+		if (Character.isDigit(wallChar)) {
+			int coinsNeeded = Character.getNumericValue(wallChar);
+			insertEdge(nodeIndex, neighbourIndex,coinsNeeded,"door");
+		}
+		else {
+			insertEdge(nodeIndex, neighbourIndex,0,"corridor");
+		}
+	}
 	/**
 	 * Insert an edge
 	 * @param node1 the first node
 	 * @param node2 the second node
-	 * @param linkType the type of the edge
+	 * @param edgeType the type of the edge
 	 * @param label the label of the edge
 	 * @throws GraphException if the edge is invalid
 	 */
-	private void insertEdge(int node1, int node2, int linkType, String label) throws GraphException {
-//		select the nodes and insert the appropriate edge.
-//		Remember that the graph is undirected, so you have to insert the edge for both nodes
-		//get the first node
-		GraphNode firstNode = graph.getNode(node1);
-		//get the second node
-		GraphNode secondNode = graph.getNode(node2);
-		//insert the edge
-		graph.insertEdge(firstNode, secondNode, linkType, label);
-		//label the edge
-		graph.insertEdge(secondNode, firstNode, linkType, label);
-		//check if the edge is a coin
-		if (linkType == coins) {
-			//increment the number of coins
-			coinsCollected++;
-		}
-		//check if the edge is the end
-		if (linkType == end) {
-			//increment the number of coins
-			coinsCollected++;
-		}
-		//check if the edge is the start
-		if (linkType == start) {
-			//increment the number of coins
-			coinsCollected++;
-		}
+	private void insertEdge(int node1, int node2, int edgeType, String label) throws GraphException {
+		GraphNode u = graph.getNode(node1);
+		GraphNode v = graph.getNode(node2);
+		graph.insertEdge(u, v, edgeType, label);
 	}
 }
